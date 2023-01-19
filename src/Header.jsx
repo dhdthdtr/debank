0b5f95eea9b8ad6f3f907f5746e1bb39a51f6153
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-01-19 09:22:14 +0700

    Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,23 +7,24 @@ function Header({handleLogout}){
     return (
         <div id="header">
             <div id="header-top">
+                <div id="blank"></div>
                 <h2 id="title">DEBANK</h2>
                 <Button size="lg" onClick={handleLogout}>Logout</Button>
             </div>
-            <nav>
+            <nav id="nav-bar">
                 <ul>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/randomTest">Tạo đề thi</Link>
-                </li>
-                <li>
-                    <Link to="/randomQuestion">Tạo câu hỏi</Link>
-                </li>
-                <li>
-                    <Link to="/news">Tin tức</Link>
-                </li>
+                    <li>
+                        <Link to="/home" className="nav-item">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/randomTest" className="nav-item">Tạo đề thi</Link>
+                    </li>
+                    <li>
+                        <Link to="/randomQuestion" className="nav-item">Tạo câu hỏi</Link>
+                    </li>
+                    <li>
+                        <Link to="/news" className="nav-item">Tin tức</Link>
+                    </li>
                 </ul>
             </nav>
         </div>
