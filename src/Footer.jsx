6d061013618c362e8d63cbc6935efb174fe7d85f
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-01-28 10:00:23 +0700

    footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 function Footer(){
     return (
-      <div>
-        
+      <div id="footer">
+        <div className="prj-member">
+          <h2 id="tag">Project Members</h2>
+          <div id="prj-member-link">
+            <a href="https://github.com/dhdthdtr">Nguyễn Vũ Bằng</a>
+            <a href="https://github.com/dhdthdtr">Nguyễn Huy Hoàng</a>
+            <a href="https://github.com/dhdthdtr">Ng Zi Hao</a>
+            <a href="https://github.com/dhdthdtr">Đỗ Cao Minh Nhật</a>
+          </div>
+        </div>
+        <div className="prj-member">
+          
+        </div>
+        <div id="source-link"></div>
       </div> 
     );
 }
