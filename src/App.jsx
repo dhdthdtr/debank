c7651fa932556d8f64d2eea8181c87c5c5d77e02
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-01-31 16:16:22 +0700

    test passing data using localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,22 @@ import Home from "./Home";
 
 function App(){
   const [success, setSuccess] = useState(false)
+  const [name, setName] = useState("");
   let navigate = useNavigate();
 
-  const handleSubmit = async () => {
+  const handleSubmit = () => {
     setSuccess(!success)
+    setName("")
     localStorage.setItem('isSuccess', JSON.stringify(success))
+    localStorage.setItem("username", JSON.stringify(name))
     navigate("home")
   }
 
   const logout = () => {
     setSuccess(!success)
+    setName("Bang")
     localStorage.setItem('isSuccess', JSON.stringify(success))
+    localStorage.setItem("username", JSON.stringify(name))
     navigate("/")
   }
 
@@ -35,7 +40,7 @@ function App(){
   }
   return (
     <div className="homepage">
-      <Header handleLogout={logout}/>
+      <Header handleLogout={logout} data={JSON.parse(localStorage.getItem("username"))}/>
           <Routes>
               <Route exact path="/" element={<Home />}></Route>
               <Route exact path="/home" element={<Home />}></Route>
