99b3ae7e4b94e3cdf19e5f1b362014b4e1bc9528
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-01-31 16:16:52 +0700

    test receive data on localStorage

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-function Header({handleLogout}){
+function Header(props){
     return (
         <header id="header">
             <div id="header-top">
@@ -13,7 +13,11 @@ function Header({handleLogout}){
                     <Link to="/randomQuestion" className="nav-item">Tạo câu hỏi</Link>               
                     <Link to="/news" className="nav-item">Tin tức</Link>
                 </nav>
-                <Button size="lg" onClick={handleLogout}>Logout</Button>
+
+                <div style={{display: 'flex'}}>
+                    <p>Chào, {props.data}</p>
+                    <Button size="lg" onClick={props.handleLogout}>Logout</Button>
+                </div>
             </div>
         </header>
     )
