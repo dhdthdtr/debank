c76643def28a9b43d2a1b75a1b4f2f2024d81ec6
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-03-06 10:08:01 +0700

    fix css News

--- src/News.jsx
+++ src/News.jsx
@@ -1,51 +1,47 @@
 import axios from "axios";
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
 
-function News(){
-    // RSS2JSON
-    const url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fvnexpress.net%2Frss%2Fgiao-duc.rss&api_key=n85bwswje5twrq6k9bkefadokwcgxgtzkhswnbvb&order_by=pubDate&count=60"
+class News extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            feedSource: []
+        }
+    }
 
-    const [feeds, setFeeds] = useState([])
-    // get title from rss using rss2json
-    useEffect(() =>{
+    componentDidMount(){
+        const url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fvnexpress.net%2Frss%2Fgiao-duc.rss&api_key=n85bwswje5twrq6k9bkefadokwcgxgtzkhswnbvb&order_by=pubDate&count=60"
+
         axios.get(url)
             .then(response => {
                 console.log(response.data.items);
-                setFeeds(response.data.items)
+                this.setState({
+                    feedSource: response.data.items
+                })
             })
-    },[])
+    }
 
-    // xử lý chuỗi description từ RSS feed
-    feeds.map(feed => {
-        var doc = new DOMParser().parseFromString(feed.description, "text/html");
-        console.log(doc.querySelector("a"))
-        console.log(feed.description)
-    })
-    
-
-    return (
-        <body id="body">
-            <div id="news-header">
-                <h2>Tin mới nhất</h2>
-                <hr />  
-            </div>
-            {feeds.map(feed => (
-                <div id="news-item-wrapper">
-                    <div id="news-title-box">
-                        <a class="news-title" href={feed.link}>{feed.title}</a>
-                    </div>
-                    <div id="news-subtitle" dangerouslySetInnerHTML={{__html: feed.description}}>
-                    </div>
+    render(){
+        let feeds = this.state.feedSource.map(feed => (
+            <div id="news-item-wrapper">
+                <div id="news-title-box">
+                    <a className="news-title" href={feed.link}>{feed.title}</a>
                 </div>
-            ))}
-            
-        </body>
-    )
+                <div id="news-subtitle" dangerouslySetInnerHTML={{__html: feed.description}}>
+                </div>
+            </div>
+        ))
+
+        return (
+            <body id="body">
+                <div id="news-header">
+                    <h2>Tin mới nhất</h2>
+                    <hr style={{zIndex: -999}}/>  
+                </div>
+                {feeds}
+            </body>
+        );
+    }
 }
 
 export default News
-
-// doc.querySelector("body").innerText -> mô tả nhỏ ở dưới tiêu đề
-// 
