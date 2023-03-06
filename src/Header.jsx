212365cd0cc6ba462e3a4c3bda5fb80dd6a33994
dhdthdtr <nguyenvubang011001hiie@gmail.com>
2023-03-06 10:06:19 +0700

    add notification box

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 function Header(props){
+    const openProfileBox = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <header id="header">
             <div id="header-top">
                 <h2 id="title">DEBANK</h2>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <div id="notification-icon">
+                        <a href="" onClick={openProfileBox} id="icon">
+                            <FontAwesomeIcon size="2x" icon={faBell}/>
+                        </a>
+                        <div id="noti-box">
+                            
+                        </div>
+                    </div>
+                    {/* <button id="logoutBtn" onClick={props.handleLogout}>Logout</button> */}
+                    Welcome,&nbsp;
+                    <a href="" onClick={openProfileBox}>Bằng Nguyễn</a>
+                </div>
+            </div>
+            <div id="nav-box">
                 <nav id="nav-bar">
                     <Link to="/home" className="nav-item">Home</Link>
                     <Link to="/randomTest" className="nav-item">Tạo đề thi</Link>               
                     <Link to="/randomQuestion" className="nav-item">Tạo câu hỏi</Link>               
                     <Link to="/news" className="nav-item">Tin tức</Link>
                 </nav>
-
-                <div style={{display: 'flex'}}>
-                    <p>Chào, {props.data}</p>
-                    <Button size="lg" onClick={props.handleLogout}>Logout</Button>
-                </div>
             </div>
         </header>
     )
